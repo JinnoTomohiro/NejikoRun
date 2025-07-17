b303e7bc2f9a55e0070fb68fb4723990fd7c0fae
JinnoTomohiro <TTC2550SG0032@edu.tech.ac.jp>
2025-07-17 11:47:29 +0900

    色々追加したよ
    具体的にはジャンプしたり重力つけたりカメラ追従するようにしたりです。

---
+++ Assets/Scripts/PlayerFollowCamera.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollowCamera : MonoBehaviour
+{
+    //カメラの位置をプレイヤーに合わせて変更する
+    Vector3 CameraPosition = Vector3.zero;
+    public GameObject followTarget;
+    public float followspeed = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //カメラと追従したいオブジェクトの距離を取得する
+        CameraPosition = followTarget.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(
+            transform.position,
+            followTarget.transform.position - CameraPosition,
+            Time.deltaTime * followspeed
+            );
+    }
+}
